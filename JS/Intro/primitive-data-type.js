4a14ec540b421ad43f742982c671e4e2c690168e
imaan <imaanally123@gmail.com>
2026-06-17 15:05:29 +0300

    learnt Primitive data types

---
+++ JS/Intro/primitive-data-type.js
@@ -0,0 +1,27 @@
+let number1=23;
+let number2=34.43;
+let number3=-34;
+let biggestNumber=6523479827161919
+console.log(biggestNumber)
+
+
+let str1="Double Qoute I am Imaan"
+console.log(str1)
+let str2='Single Qoute I am Imaan'
+console.log(str2)
+let str3=Back Tick I am Imaan
+console.log(str3)
+
+let str4="23"
+console.log("23 is a string Dont")
+let str5='Imaan said "23 is a Number"'
+
+let str6=`
+    multi line string 
+    multi line strings 
+    ""'' both single and double qoutes 
+    Dont 
+    `
+
+let str7='Dont't do this anymore'
+
