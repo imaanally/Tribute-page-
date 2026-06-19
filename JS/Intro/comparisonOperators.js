32ccff1acf9b4cde7d38d6c56772c88b1299351d
imaan <imaanally123@gmail.com>
2026-06-19 09:57:27 +0300

    Learnt Comparison Operators

---
+++ JS/Intro/comparisonOperators.js
@@ -0,0 +1,28 @@
+//comparison only evaluate to true or false 
+//1. equality ==
+//2.strict equality===
+//3.inequality !=
+//4. strict inequality !==
+
+let numb1="23"
+let numb2=23
+let strictEqual=numb1===numb2
+console.log(`numb1=${numb1}===numb2=$numb2=${numb2}
+    Ans ${strictEqual} its type is ${typeof strictEquality}`)
+let nonStrictEquality=numb1==numb2
+console.log(`numb1=${numb1}===numb2=$numb2=${numb2}
+    Ans ${nonStrictEquality} its type is ${typeof nonStrictEquality}`);
+
+
+let val1="30"
+let val2=30
+//different should not be equal
+let strictInequality=val1!==val2
+console.log(`val1=${val1}!==val2=${val2}
+    Ans ${strictInequality} its type is ${typeof strictInequality}`);
+
+let Inequality=val1!=val2
+console.log(`val1=${val1}!=val2=${val2}
+    Ans ${Inequality} its type is ${typeof Inequality}`);
+
+
