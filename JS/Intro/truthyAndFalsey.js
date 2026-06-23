25b387be4a0a9ea590125f56b614d01db8023899
imaan <imaanally123@gmail.com>
2026-06-23 10:31:22 +0300

    learnt truthy and falsey

---
+++ JS/Intro/truthyAndFalsey.js
@@ -0,0 +1,24 @@
+/*
+Truthy or Falsey 
+if you know something is not falsey then it is truthy 
+falsey
+1. false
+2. 0
+3.-0
+4. null
+5. undefined
+6. "" empty string
+7. NoN -> not a number 
+anything that is not falsey is truthy 
+truthy || falsey -> truthy 
+*/
+
+let userName1="John Mwangi" //truthy
+let userName2=""  // falsey
+
+let activeUserName=userName2 || userName1
+
+console.log(activeUserName)
+
+let bothUserNameOk=userName1&&userName2 
+console.log(bothUserNameOk)  // truthy or falsey 
