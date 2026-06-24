7f1caa59bbe2025764fe51f71b6f6412d0bfccd8
imaan <imaanally123@gmail.com>
2026-06-24 10:45:34 +0300

    learnt if only

---
+++ JS/Intro/ifOnly.js
@@ -0,0 +1,38 @@
+/*
+lets start with if 
+its special 
+you can use it on its own 
+*/
+
+let age=prompt ("enter your age");
+alert(`you entered ${age}`)
+
+
+
+if(age>10){
+    alert("you are a baby")
+}
+
+if(age>16){
+     alert("you are a teen");
+}
+
+if(age>18){
+    alert("you can legally drink and smoke");
+    alert("but should you?");
+}
+
+if(age>27){
+    alert("best age for marriage");
+    alert("get married");
+}
+
+if(age>45){
+    alert("best age for retirement");
+    alert("get a second wife");
+}
+
+if(age>50){
+    alert("you are an ancestor");
+}
+
