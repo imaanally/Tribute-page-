df303d4cb4767e9d6225e488285d1398b550d086
imaan <imaanally123@gmail.com>
2026-06-24 10:09:06 +0300

    learnt if statement

---
+++ JS/Intro/condition.js
@@ -0,0 +1,61 @@
+/*
+scenario 1;
+1. declare a variable of your choice assign a truthy value 
+create an if statement and put the variable 
+inside the if statement condition (<variable>) inside if console log 
+("scenario 1 it run")
+scenario 2 
+2. declare a variable of your choice assign a falsey value 
+create an if statement and put the variable 
+inside the if statement condition (<variable>) inside if console log 
+("scenario 1 it run")
+scenario 3 
+3. create an if statement inside the if statement condition part 
+have true
+inside block ({}) console log ("scenario 3 it run")
+scenario 4 
+4. create an if statement inside the if statement condition part 
+have false
+inside block ({}) console log ("scenario 4 it run")
+scenario 5 
+create an if statement inside the if statement condition part 
+have 10>20
+inside block ({}) console log ("scenario 5 it run")
+scenario 6 
+create an if statement inside the if statement condition part 
+have 20>10
+inside block ({}) console log ("scenario 6 it run")
+*/
+
+//scenario 1
+
+let myChoice=12
+if(myChoice){
+      console.log("scenario 1 it run")
+}
+
+// scenario 2 
+let myChoice2=-0
+if(myChoice2){
+    console.log("scenario 2 it run");
+}
+
+//scenario 3 
+if(true){
+    console.log("scenario 3 it run")
+}
+
+//scenario 4 
+if (false) {
+  console.log("scenario 4 it run");
+}
+
+//scenario 5 
+if(10>20){
+    console.log("scenario 5 it run")
+}
+
+//scenario 6 
+if (20>10) {
+  console.log("scenario 6 it run");
+}
--- JS/Intro/truthyAndFalsey.js
+++ JS/Intro/truthyAndFalsey.js
@@ -4,11 +4,11 @@ if you know something is not falsey then it is truthy
 falsey
 1. false
 2. 0
-3.-0
+3. -0
 4. null
 5. undefined
 6. "" empty string
-7. NoN -> not a number 
+7. NaN -> not a number 
 anything that is not falsey is truthy 
 truthy || falsey -> truthy 
 */
