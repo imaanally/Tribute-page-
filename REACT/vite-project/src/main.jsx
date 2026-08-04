8be2dd9fcb6d86f9123ea36afe9ed84080f88732
imaan <imaanally123@gmail.com>
2026-08-04 14:16:41 +0300

    learnt state and events

--- REACT/vite-project/src/main.jsx
+++ REACT/vite-project/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
-import AmazonPage2 from "./AmazonPage2.0"
+import StatendEvents from "./StateAndEvents/Form"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AmazonPage2/>
+    <StateAndEvents/>
   </StrictMode>,
 );
 
