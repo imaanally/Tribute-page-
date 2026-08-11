b9dea216e3bd6995dc2e4c7b8a94a561555d47f5
imaan <imaanally123@gmail.com>
2026-08-11 10:24:29 +0300

    Implement React Router with navigation

--- REACT/vite-project/src/Routing/index.jsx
+++ REACT/vite-project/src/Routing/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
