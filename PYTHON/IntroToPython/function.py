7b77b6e81820e8d57c01660a9874bbab3b6a0264
imaan <imaanally123@gmail.com>
2026-08-26 10:29:49 +0300

    Learnt Condition and loops And functions in python

---
+++ PYTHON/IntroToPython/function.py
@@ -0,0 +1,17 @@
+#function areaCircle(radius)
+#{ return 3.142*radius*radius}
+
+#def-> pi
+
+def area_circle(radius):
+    ans= 3.142*pow(radius,2)
+    print ("for circle of radius {radius} area is {ans}")
+    return
+
+area_circle (20)
+
+new_f=area_circle
+
+new-f(10)
+
+
